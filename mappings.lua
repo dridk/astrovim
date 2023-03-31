8349eb677b7024a432a1d312a19080f88c823702
dridk <sacha@labsquare.org>
2023-04-01 01:05:35 +0200

    move line

--- mappings.lua
+++ mappings.lua
@@ -3,7 +3,8 @@ return {
   n = {
     ["<S-tab>"] = { ':bnext<cr>', desc = "next buffer" },
     ["<C-x>"] = { ':Bdelete', desc = "Close buffer" },
-    ["<C-d>"] = {':%s/<C-r><C-w>//g<left><left>', desc=""}
-
+    ["<C-d>"] = {':%s/<C-r><C-w>//g<left><left>', desc=""},
+  ["<C-up>"] = {':m -2 <cr>', desc="move line up"},
+    ["<C-down>"] = {':m +1<cr>', desc="move line down"}
   }
 }
