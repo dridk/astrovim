8e6e0792f98f0e6a24e27e6be0e4324a10ca8872
dridk <sacha@labsquare.org>
2023-04-01 00:51:18 +0200

    Add replace shortcut

--- mappings.lua
+++ mappings.lua
@@ -2,7 +2,8 @@
 return {
   n = {
     ["<S-tab>"] = { ':bnext<cr>', desc = "next buffer" },
-    ["<C-x>"] = { ':Bdelete', desc = "Close buffer" }
+    ["<C-x>"] = { ':Bdelete', desc = "Close buffer" },
+    ["<C-d>"] = {':%s/<C-r><C-w>//g<left><left>', desc=""}
 
   }
 }
