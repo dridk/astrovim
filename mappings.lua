3a5ca1094199a236b96937ec51ef9a4539fa17ab
dridk <sacha@labsquare.org>
2023-07-25 22:39:18 +0200

    update

--- mappings.lua
+++ mappings.lua
@@ -1,10 +1,15 @@
 
 return {
   n = {
-    ["<S-tab>"] = { ':bnext<cr>', desc = "next buffer" },
-    ["<C-x>"] = { ':Bdelete', desc = "Close buffer" },
-    ["<C-d>"] = {':%s/<C-r><C-w>//g<left><left>', desc=""},
-  ["<C-up>"] = {':m -2 <cr>', desc="move line up"},
-    ["<C-down>"] = {':m +1<cr>', desc="move line down"}
+    ["<tab>"] = { ':b', desc = "next buffer" },
+    ["<S-tab>"] = { ':b#<cr>', desc = "next buffer" },
+    ["<C-x>"] = { ':Bdelete<cr>', desc = "Close buffer" },
+    ["<C-d>"] = {':%s/<C-r><C-w>//g<left><left>', desc="replace current word"},
+
+  },
+
+  i = {
+    ["<C-l>"] = {"<esc>ddi", desc="delete line"},
+    ["<C-d>"] = {'<esc>yypi', desc="duplicate line"},
   }
 }
